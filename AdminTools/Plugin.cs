66544c5fab3242680258ff36c0eb886795d366e0
Axwabo <axwabo@gmail.com>
2023-10-28 09:50:31 +0200

    update PluginAPI

--- AdminTools/Plugin.cs
+++ AdminTools/Plugin.cs
@@ -18,7 +18,7 @@ namespace AdminTools
     {
         public const string Author = "Galaxy119, Neil, Axwabo";
         public const string Name = "Admin Tools";
-        public const string Version = "2.4.3";
+        public const string Version = "2.4.4";
 
         public EventHandlers EventHandlers;
 
